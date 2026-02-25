e7af2cb2d756006dbd0bc22a330abd89be931010
RFN <matveyr0dionov@yandex.ru>
2026-02-25 23:48:14 +0300

    update soon

--- asi/Achievement/source/Main.cpp
+++ asi/Achievement/source/Main.cpp
@@ -1,25 +1,27 @@
 #include <plugin.h> // Plugin-SDK version 1002 from 2025-12-09 23:18:09
 #include <CMessages.h>
+#include <CMenuManager.h>
+#include <Windows.h>
+#include <iostream>
 
 using namespace plugin;
 
 struct Main
 {
-    size_t m_frame = 0; // render frame counter
+    bool isDone;
 
     Main()
     {
-        // register event callbacks
-        Events::gameProcessEvent += []{ gInstance.OnGameProcess(); };
+        Events::menuDrawingEvent += [this]() { DrawMenu(); };
     }
 
-    void OnGameProcess()
-    {
-        m_frame++;
-        
-        static char msg[255];
-        sprintf_s(msg, "Hello from '%s' plugin! Frame %d", TARGET_NAME, m_frame);
-        
-        CMessages::AddMessageJumpQ(msg, 500, 0);
+    void DrawMenu() {
+        CMenuManager* menu = &FrontEndMenuManager;
+        if ((menu->m_bMenuActive && menu->m_nCurrentMenuPage == MENUPAGE_OPTIONS) && isDone == false) {
+            menu-> // update soon =)
+        }
     }
-} gInstance;
+
+};
+
+Main mainInstance;
