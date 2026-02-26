16977c6fab71485635664b49e22efa3770b69fb7
RFN <matveyr0dionov@yandex.ru>
2026-02-27 00:02:44 +0300

    updated!

--- asi/Achievement/source/Main.cpp
+++ asi/Achievement/source/Main.cpp
@@ -1,9 +1,11 @@
-#include <plugin.h> // Plugin-SDK version 1002 from 2025-12-09 23:18:09
+#include <plugin.h>
 #include <CMessages.h>
 #include <CMenuManager.h>
 #include <Windows.h>
 #include <string>
 #include <CFont.h>
+#include <CSprite2d.h>
+#include <CTxdStore.h>
 #define SCM_GLOBALS_BASE 0xA49960
 
 using namespace plugin;
@@ -22,57 +24,68 @@ int GetSCMGlobal(int index) {
 
 struct AchievementOption
 {
-    bool isDone = false;
     bool bInited = false;
-
+    static CSprite2d ach_car;
     AchievementOption()
     {
         Events::initRwEvent += [this](){
             if (!bInited) {
                 SetupNewPage();
                 InitPage();
+
+                int ach_txd = CTxdStore::AddTxdSlot("ach_txd");
+
+                CTxdStore::LoadTxd(ach_txd, "MODELS\\ACHV.TXD");
+                CTxdStore::AddRef(ach_txd);
+                CTxdStore::PushCurrentTxd();
+                CTxdStore::SetCurrentTxd(ach_txd);
+                
+                ach_car.SetTexture("ach1", NULL);
+                CTxdStore::PopCurrentTxd();
+
                 bInited = true;
             }
         };
+        Events::menuDrawingEvent += [this]() {
+            RenderAchievements();
+        };
+
+        Events::shutdownRwEvent += [] {
+            ach_car.Delete();
+            CTxdStore::RemoveTxdSlot(CTxdStore::FindTxdSlot("ach_txd"));
+        }
+
+    }
+
+    void RenderAchievements() {
+        if (FrontEndMenuManager.m_nCurrentMenuPage == MENUPAGE_BRIEFS) {
+        }
     }
 
     void SetupNewPage() {
-
         CMenuScreen* ac_page = &aScreens[MENUPAGE_BRIEFS];
-
         memset(ac_page, 0, sizeof(CMenuScreen));
-
         FrontEndMenuManager.m_nNumberOfMenuOptions = 2;
         FrontEndMenuManager.m_nCurrentMenuEntry = 0;
-
         strncpy(ac_page->m_ScreenName, "FEP_BRI", 8);
-
-        ac_page->m_nPreviousPage = MENUPAGE_MAIN_MENU;
+        ac_page->m_nPreviousPage = MENUPAGE_PAUSE_MENU;
         ac_page->m_nParentEntry = 0;
-
-
-        ac_page->m_aEntries[0].m_nAction = MENUACTION_BACK_PC;
+        ac_page->m_aEntries[0].m_nAction = MENUACTION_BACK;
         strncpy(ac_page->m_aEntries[0].m_EntryName, "FEDS_TB", 8);
-
-        ac_page->m_aEntries[0].m_nX = 150;
-        ac_page->m_aEntries[0].m_nY = 190;
-
+        ac_page->m_aEntries[0].m_nX = 130;
+        ac_page->m_aEntries[0].m_nY = 360;
         ac_page->m_aEntries[0].m_nAlign = 0;
-
-        ac_page->m_aEntries[1].m_nAction = MENUACTION_BACK_PC;
-        strncpy(ac_page->m_aEntries[1].m_EntryName, "FED_DBG", 8); 
-        ac_page->m_aEntries[1].m_nAlign = 0;
-        
-        
     }
 
     void InitPage() {
-        CMenuScreen* pausePage = &aScreens[MENUPAGE_MAIN_MENU];
-
+        CMenuScreen* pausePage = &aScreens[MENUPAGE_PAUSE_MENU];
         int slotindex = 4; // 5
         pausePage->m_aEntries[slotindex].m_nAction = MENUACTION_CHANGEMENU;
         pausePage->m_aEntries[slotindex].m_nTargetMenu = MENUPAGE_BRIEFS;
         strncpy(pausePage->m_aEntries[slotindex].m_EntryName, "FEP_BRI", 8);
+        pausePage->m_aEntries[slotindex + 1].m_nAction = MENUACTION_CHANGEMENU;
+        pausePage->m_aEntries[slotindex + 1].m_nTargetMenu = MENUPAGE_EXIT;
+        strncpy(pausePage->m_aEntries[slotindex + 1].m_EntryName, "FEP_QUI", 8);
     }
 
 } mainInstance;
